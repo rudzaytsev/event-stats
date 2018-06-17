24477b9c07d7f9c27b79cc43d141e0b54dec4014
rudzaytsev <rudzaytsev@gmail.com>
2018-06-17 20:53:36 +0300

    Added more tests for counting events of last minute.

--- src/test/java/org/events/EventStatsCounterTest.java
+++ src/test/java/org/events/EventStatsCounterTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doReturn;
@@ -31,6 +33,33 @@ public class EventStatsCounterTest {
   public void recentlyRegisteredEventShouldBeCounted() {
     LocalDateTime eventTime = now.minusSeconds(1);
     stats.registerEvent(eventTime);
-    assertEquals(1, stats.countEventsOfLastMinute());
+    assertEquals(1L, stats.countEventsOfLastMinute());
+  }
+
+  @Test
+  public void eventsOfLastMinuteShouldBeCounted() throws Exception {
+    List<LocalDateTime> eventTimes = Arrays.asList(
+      now.minusSeconds(5), now.minusSeconds(59),
+      now.minusMinutes(1), now.minusMinutes(3),
+      now.minusHours(1)
+    );
+    registerEvents(eventTimes);
+    assertEquals(2L, stats.countEventsOfLastMinute());
+  }
+
+  @Test
+  public void eventsOfLastMinutedShouldBeZeroIfAllEventsAreTooOld() throws Exception {
+    List<LocalDateTime> oldEventTimes = Arrays.asList(
+      now.minusSeconds(60), now.minusMinutes(2),
+      now.minusMinutes(5),  now.minusHours(3)
+    );
+    registerEvents(oldEventTimes);
+    assertEquals(0L, stats.countEventsOfLastMinute());
+  }
+
+  private void registerEvents(List<LocalDateTime> oldEventTimes) {
+    for (LocalDateTime eventTime : oldEventTimes) {
+      stats.registerEvent(eventTime);
+    }
   }
 }
